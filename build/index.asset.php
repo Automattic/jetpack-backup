3e631d6c5418d47336dbd3c8487d2cafc5b5fdc7
bindlegirl <bindlegirl@users.noreply.github.com>
2023-02-15 10:44:35 +0000

    Blaze: abort early if JP not connected and defensive coding re get_current_screen (#28955)
    * Blaze: abort early if JP not connected and defensive coding re get_current_screen
    
    * Flip order of checks per code review
    
    * We need spacing! Spacing I say
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4182901900

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '50ac69a6bf48994196e3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ea81dfe7b3b13ca179c8');
